08265479857432515594a458e45e148a90b70498
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-03 22:05:55 +0530

    day2_variables.cpp

---
+++ cpp/basics/variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+/*programs to demonstrate basic variable declaration and update in c++*/
+int main()
+{
+  int number;
+  number=3;
+  cout << "intial value"<< number << endl;
+  number=4;
+  cout << "updated value" << number << endl;
+  return 0;
+  }
