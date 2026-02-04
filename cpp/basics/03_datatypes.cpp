3c19a83f9cf235cdf4940202528f4c7329cc959f
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-04 19:04:57 +0530

    day3_datatypes.cpp

---
+++ cpp/basics/03_datatypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+/*program to demonstrate basic datatypes in c++*/
+int main()
+{
+  int Age ;
+  float Height ;
+  char Name;
+  cin >> Name >> Age >> Hieght ;
+  cout << "Hello," << Name << "May I know your age."<< endl;
+  cout << "Hi,my age is" << Age << endl;
+  cout << "what's your Height ?" << endl;
+  cout << "it's" << Height << endl;
+  return 0;
+}
