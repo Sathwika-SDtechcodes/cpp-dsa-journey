9035ada24e99f9dddaaf83fa95f611e3e21e0664
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-05 20:01:15 +0530

    day4_unary.cpp

---
+++ cpp/operators/05_unary.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//Demonstrate unary operators (increment and decrement)
+int main()
+{
+  int a;
+  cin >> a ;
+  cout << (++a) << endl;
+  cout << (a++) << endl;
+  cout << (--a) << endl;
+  cout << (a--) << endl;
+  return 0;
+}
