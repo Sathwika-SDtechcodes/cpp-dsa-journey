f4a080326d98c4e925888fcef19cd77c81a31b53
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-05 18:57:09 +0530

    day4_relational.cpp

---
+++ cpp/operators/02_relational.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+/*Demonstrate relational operators*/
+int main()
+{
+  int a = 50,b =30;
+  cout << (a==b) << "a and b are equal" << endl;
+  cout << (a<=b) << "a is less than or equal to b"<< endl;
+  cout << (a>=b) << "a is greater than or equal to b"<< endl;
+  cout << (a!=b) << "a is not equal to b" << endl;
+  cout << (a>b) << "a is greater than b" << endl;
+  cout << (a<b) << "a is less than b" << endl;
+  return 0;
+}
