465efa2cd0fe120d2b187adfc5b8df53bd5cc1fa
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-05 08:02:47 +0530

    day4_arithmetic.cpp

---
+++ cpp/operators/01_arithmetic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+/*demonstrate arithmetic operators*/
+int main()
+{ 
+  int a = 50 , int b = 30;
+  cout << "addition" << a+b << endl;
+  cout << "subtraction" << a-b << endl;
+  cout << "division" << a/b << endl;
+  cout  << "modulus" << a%b << endl;
+  cout << "multiplication" << a*b << endl;
+  return 0;
+}
