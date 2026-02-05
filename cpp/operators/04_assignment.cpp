998661da2f772b8c0046eaf043e9aeb1d3363239
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-05 19:50:41 +0530

    day4_assignment.cpp

---
+++ cpp/operators/04_assignment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Demonstrate Assignment operators
+int main()
+{
+  int x ;
+  cin >> x;
+  x += 5;
+  x -= 2;
+  x *= 4;
+  x /= 2;
+  cout << "now x value is :" << x << endl; 
+  return 0;
+}
