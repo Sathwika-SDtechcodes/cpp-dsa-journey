9d044d8f6387fb9397218fea4630a054478c0f78
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-06 20:10:57 +0530

    day5_if-condition.cpp

---
+++ cpp/conditions/01_if-condition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Demonstrates simple if statement
+int main()
+{
+  int percentage;
+  cout <<"Enter the percentage of the attendence :" << endl;
+  cin >> percentage;
+  if(percentage >= 75 %)
+  { 
+    cout << "you are eligible to promote for the next year ." << endl;
+  }
+  return 0;
+}
