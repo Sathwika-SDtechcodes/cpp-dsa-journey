3897fff5f42cf6d35d4f0351961f1708d148dcf5
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-08 20:17:31 +0530

    day3_datatypes.cpp

--- cpp/basics/03_datatypes.cpp
+++ cpp/basics/03_datatypes.cpp
@@ -5,11 +5,16 @@ int main()
 {
   int Age ;
   float Height ;
-  char Name;
-  cin >> Name >> Age >> Height ;
-  cout << "Hello," << Name << "May I know your age."<< endl;
-  cout << "Hi,my age is" << Age << endl;
-  cout << "what's your Height ?" << endl;
-  cout << "it's" << Height << endl;
+  string Name;
+  cout << "Enter your Full Name :" << endl;
+  getline(cin,Name);
+  cout << "Enter your Age" << endl;
+  cin >>  Age ;
+  cout << "Enter the Height" << endl;
+  cin >> Height;
+  cout << "Hello," << Name << "! May I know your age?" << endl;
+  cout << "Hi,my age is " << Age << endl;
+  cout << "what's your Height?" << endl;
+  cout << "it's " << Height << " meters" << endl;
   return 0;
 }
