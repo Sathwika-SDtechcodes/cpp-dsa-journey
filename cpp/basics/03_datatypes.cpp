953101fbc2879ba9147cafd7a620e8ae3d4d2325
Sathwika-SDtechcodes <sakesathwikamanohar@gmail.com>
2026-02-08 19:54:16 +0530

    day3_datatypes.cpp

--- cpp/basics/03_datatypes.cpp
+++ cpp/basics/03_datatypes.cpp
@@ -6,7 +6,7 @@ int main()
   int Age ;
   float Height ;
   char Name;
-  cin >> Name >> Age >> Hieght ;
+  cin >> Name >> Age >> Height ;
   cout << "Hello," << Name << "May I know your age."<< endl;
   cout << "Hi,my age is" << Age << endl;
   cout << "what's your Height ?" << endl;
